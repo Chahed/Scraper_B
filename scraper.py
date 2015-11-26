d8f03817f433d38b426fb1eb56b714c1038f7993
Hedi Chahed <hedi.chahed.2i@gmail.com>
2015-11-26 02:09:48 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,11 @@ def suittext(text):
     a=text.split(" ")
     d=a[0].replace("\\t","")
     a=d.replace("\\n","")
-    return a
+    l=""
+    for i in range(0,len(a)-1) :
+        if a[i] in ['1','2','3','4','5','6','7','8','9','0','-']:
+            l+=a[i]
+    return l
 
 def dateclean(date):
     date=suittext(date)
